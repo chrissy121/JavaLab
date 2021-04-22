6cb43e718cc73c905f87eef8bdceaff01abce1e6
Chrissy121 <crys121@gmail.com>
2021-04-22 09:06:53 -0400

    First commit

---
+++ src/OfficeHours/Mine/ArrayPractice.java
@@ -0,0 +1,13 @@
+package OfficeHours.Mine;
+
+public class ArrayPractice {
+    public static void main(String[] args) {
+
+        int nums1 = 1;
+        int nums2 = 2;
+        int nums3 = 3;
+
+        int[] numbers = new int[8];
+        System.out.println(numbers);
+    }
+}
