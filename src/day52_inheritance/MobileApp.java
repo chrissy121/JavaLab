9615b92b0d4e84000425ee3a009733f691280076
Chrissy121 <crys121@gmail.com>
2021-06-12 15:15:02 -0400

    mobile app example third

--- src/day52_inheritance/MobileApp.java
+++ src/day52_inheritance/MobileApp.java
@@ -5,6 +5,11 @@ public class MobileApp {
     String name;
     double version;
 
+    protected boolean download() {
+    System.out.println("App: " + name + " version: " + version + " is downloaded.");
+    return true;
+   }
+
     public void useTheApp(int minutes) {
         System.out.println("Using " + name + "this app for " + minutes + "minutes");
     }
